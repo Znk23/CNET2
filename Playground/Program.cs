0346943f200caacf0ffee2c5190f90c1ca01dc5b
Znk23 <94828778+Znk23@users.noreply.github.com>
2021-11-22 11:24:02 +0100

    3

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,20 +1,25 @@
 ﻿Console.WriteLine("Hello, World!");
 
 
-var numbers = new[] { 2, 6 }; //{ 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
 // string - pomocí LINQu pomocí Select vytvořte nové pole kde jsou všechna slova uppercase
 
 //1
-var result = strings.Select(x => x.ToUpper());
+//var result = strings.Select(x => x.ToUpper());
 
 // 2 zjistěte pomocí LINQu jestli pole obsahuje pouze sudá čísla
 bool isOnlyEvenNumbers = numbers.All(x => x % 2 == 0);
-
 global::System.Console.WriteLine($"jsou vsechna cisla suda: {isOnlyEvenNumbers}");
 
-//PrintList(result.ToList());
+// 3  vypsat čísla v poli numbers jako anglicka slova
+
+var result = numbers.Select(x => strings[x]);
+
+   
+
+PrintList(result.ToList());
 
 
 static void PrintList(List<string> listToPrint)
