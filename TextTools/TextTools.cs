274f1515b48c97000c79c2924e4f93a5f93133cb
Zdeněk Oreský <zdenek.oresky@gmail.com>
2021-11-24 09:23:48 +0100

    app gui async

---
+++ TextTools/TextTools.cs
@@ -0,0 +1,49 @@
+﻿namespace TextTools
+{
+    public class TextTools
+    {
+        public static async Task<Dictionary<string, int>> FreqAnalysisFromFileAsync(string file, string splitby = " ")
+        {
+            var content = await File.ReadAllTextAsync(file);
+            return FreqAnalysisFromString(content, splitby);
+        }
+
+        public static Dictionary<string, int> FreqAnalysisFromFile(string file, string splitby = " ")
+        {
+            var content = File.ReadAllText(file);
+            return FreqAnalysisFromString(content);
+        }
+
+        public static Dictionary<string, int> FreqAnalysisFromString(string content, string splitby = " ")
+        {
+            var words = content.Split(splitby);
+
+            Dictionary<string, int> dict = new();
+
+            foreach (var word in words)
+            {
+                if (string.IsNullOrWhiteSpace(word))
+                    continue;
+
+                if (dict.ContainsKey(word))
+                {
+                    dict[word] = dict[word] + 1;
+                }
+                else
+                {
+                    dict.Add(word, 1);
+                }
+            }
+
+            return dict;
+        }
+
+        public static Dictionary<string, int> GetTopWords(int takeTop, Dictionary<string, int> dict)
+        {
+            return dict
+                .OrderByDescending(x => x.Value).Take(takeTop)
+                .ToDictionary(x => x.Key, y => y.Value);
+            ;
+        }
+    }
+}
---
+++ WPFTextGUI/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFTextGUI.Model;
+
+namespace WPFTextGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string bigfilesdir = @"C:\Users\zdene\source\repos\Znk23\CNET2\BigFiles";
+
+        static IEnumerable<string> GetBigFiles()
+        {
+            return Directory.EnumerateFiles(bigfilesdir, "*.txt");
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private async void LoadBooks()
+        {
+            //var bookdir = @"C:\Users\Student\source\repos\CNET2\Books";
+
+            //foreach (var file in GetFilesFromDir(bookdir))
+            //{
+            //    var dict = await TextTools.TextTools.FreqAnalysisAsync(file);
+            //    var top10 = TextTools.TextTools.GetTopWords(10, dict);
+            //    var fi = new FileInfo(file);
+
+            //    txbInfo.Text += fi.Name + Environment.NewLine;
+            //    foreach (var kv in top10)
+            //    {
+            //        txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+            //    }
+            //    txbInfo.Text += Environment.NewLine;
+            //}
+        }
+       
+        private async void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            txbInfo.Text = txbDebugInfo.Text = "";
+            Mouse.OverrideCursor = Cursors.Wait;
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            var files = GetBigFiles();
+            
+            foreach(var file in files)
+            {
+                var wordsstats = await TextTools.TextTools.FreqAnalysisFromFileAsync(file, Environment.NewLine);
+                var top10 = TextTools.TextTools.GetTopWords(10, wordsstats);
+
+                var fi = new FileInfo(file);
+                txbInfo.Text += fi.Name + Environment.NewLine;
+                foreach (var kv in top10)
+                {
+                    txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+                }
+                txbInfo.Text += Environment.NewLine;
+                txbDebugInfo.Text += stopwatch.ElapsedMilliseconds + Environment.NewLine;
+
+                Data.Data.Results.Add(new StatsResult() { Source = file, Top10Words = top10 });
+
+                progress1.Value += 100.0 / files.Count();
+            }
+
+            stopwatch.Stop();
+            txbDebugInfo.Text = "elapsed ms: " + stopwatch.ElapsedMilliseconds;
+            Mouse.OverrideCursor = null;
+        }
+
+        private void btnStatsAll_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            txbInfo.Text = txbDebugInfo.Text = "";
+
+            var files = GetBigFiles();
+
+            var allwords = 
+                string.Join(Environment.NewLine, 
+                files.Select(f => File.ReadAllText(f)));
+
+            var dict = TextTools.TextTools.FreqAnalysisFromString(allwords, Environment.NewLine);
+            var top10 = TextTools.TextTools.GetTopWords(10, dict);
+
+            foreach (var kv in top10)
+            {
+                txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+            }
+
+            stopwatch.Stop();
+            txbDebugInfo.Text = "elapsed ms: " + stopwatch.ElapsedMilliseconds;
+            Mouse.OverrideCursor = null;
+        }
+
+        private void btnStatsAllParallel_Click(object sender, RoutedEventArgs e)
+        {
+            txbInfo.Text = txbDebugInfo.Text = "";
+            Mouse.OverrideCursor = Cursors.Wait;
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            ConcurrentDictionary<string, int> dict = new();
+
+            var files = GetBigFiles();
+
+            Parallel.ForEach(files, file =>
+            {
+                foreach(var word in File.ReadAllLines(file))
+                {
+                    dict.AddOrUpdate(word, 1, (key, oldValue) => oldValue + 1);
+                }
+            });
+
+            foreach (var kv in dict.OrderByDescending(x => x.Value).Take(10))
+            {
+                txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+            }
+
+            stopwatch.Stop();
+            txbDebugInfo.Text = "elapsed ms: " + stopwatch.ElapsedMilliseconds;
+            Mouse.OverrideCursor = null;
+        }
+
+        private void btnStatsAllParallelLock_Click(object sender, RoutedEventArgs e)
+        {
+            txbInfo.Text = txbDebugInfo.Text = "";
+            Mouse.OverrideCursor = Cursors.Wait;
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            object locker = new object();
+            Dictionary<string, int> dict = new();
+
+            var files = GetBigFiles();
+
+            Parallel.ForEach(files, file =>
+            {
+                foreach (var word in File.ReadAllLines(file))
+                {
+                    lock (locker)
+                    {
+                        if (dict.ContainsKey(word))
+                            dict[word]++;
+                        else
+                            dict.Add(word, 1);
+                    }
+                }
+            });
+
+            foreach (var kv in dict.OrderByDescending(x => x.Value).Take(10))
+            {
+                txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+            }
+
+            stopwatch.Stop();
+            txbDebugInfo.Text = "elapsed ms: " + stopwatch.ElapsedMilliseconds;
+            Mouse.OverrideCursor = null;
+        }
+    }
+}
